2c6e023714d7bee3db94b3108f0f14026bf381fa
Greg <gskril@gmail.com>
2023-02-14 12:04:12 -0500

    Remove unused code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,12 @@
-import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-import '@rainbow-me/rainbowkit/styles.css'
 import PlausibleProvider from 'next-plausible'
 import type { AppProps } from 'next/app'
-import { WagmiConfig } from 'wagmi'
 
-import { chains, wagmiClient } from '../providers'
 import '../styles/style.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <PlausibleProvider domain="example.com" trackOutboundLinks>
-      <WagmiConfig client={wagmiClient}>
-        <RainbowKitProvider chains={chains} modalSize="compact">
-          <Component {...pageProps} />
-        </RainbowKitProvider>
-      </WagmiConfig>
+      <Component {...pageProps} />
     </PlausibleProvider>
   )
 }
